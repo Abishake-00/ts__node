8b7cfaa400caff9bc8bc419b982387e2f21a1d1f
abdul Rahman <rah07abd2001@gamil.com>
2025-10-13 13:54:06 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { BASE_PATH, ENV } from "./config/index";
 import { HHH } from "./routes";
+import cron from "node-cron";
+import { AttendanceService } from "@modules/attendance/service/attendance.service";
 
 const app = express();
 
@@ -11,6 +13,42 @@ const port = Number(process.env.PORT) || 3000;
 
 app.get("/health", (_req, res) => {
     res.json({ status: "ok" });
+});
+const attendanceService = new AttendanceService("",10000);
+
+cron.schedule("0 0 1 * *", async () => {
+  console.log("🕛 Running monthly attendance cron job...");
+  try {
+    attendanceService.generateMonthlyAttendance()
+    console.log("✅ Monthly attendance generated successfully!");
+  } catch (error) {
+    console.error("❌ Error generating attendance:", error);
+  }
+});
+function getRandomMinute(startMinute: number, endMinute: number): number {
+  return Math.floor(Math.random() * (endMinute - startMinute)) + startMinute;
+}
+
+// 1️⃣ Morning check-in (random 09:50–10:05)
+const checkInMinute = getRandomMinute(50, 55); // 50..54
+cron.schedule(`${checkInMinute} 9 * * 1-5`, async () => {
+  console.log(`🕘 Running check-in cron at random minute ${checkInMinute}`);
+  try {
+    await attendanceService.loginAllUsers("in");
+  } catch (error) {
+    console.error("❌ Error in check-in cron:", error);
+  }
+});
+
+// 2️⃣ Evening check-out (random 18:00–18:10)
+const checkOutMinute = getRandomMinute(0, 10); // 0..9
+cron.schedule(`${checkOutMinute} 18 * * 1-5`, async () => {
+  console.log(`🕕 Running check-out cron at random minute ${checkOutMinute}`);
+  try {
+    await attendanceService.loginAllUsers("out");
+  } catch (error) {
+    console.error("❌ Error in check-out cron:", error);
+  }
 });
 
 app.listen(port, "0.0.0.0", () => {
